ed3d79f15d41e2d17b4eb2c4cd7cb7613e2c24aa
Jefferson A <jabarca840@gmail.com>
2024-12-23 11:45:04 -0600

    3

--- PROYECTO1_agranados_jabarca/PROYECTO1_agranados_jabarca.cpp
+++ PROYECTO1_agranados_jabarca/PROYECTO1_agranados_jabarca.cpp
@@ -4,7 +4,7 @@ using namespace std;
 int main()
 {
     int dylanLikes = 10;
-    cout << "A dilan le gusgatan las de " << dylanLikes << endl;
+    cout << "A dilan le gusgtan las de " << dylanLikes << endl;
     cout << "Hello World!\n";
     cout << "Bye world" << endl;
     cout << "Me gusta Dylan mucho " << endl;
